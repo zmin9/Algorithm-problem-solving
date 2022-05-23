fcfc1e6243c137492867d239f417e782b727b525
cje1903 <86587287+cje1903@users.noreply.github.com>
2022-05-23 11:33:04 +0900

    05/23/월

--- jieun^,^/BOJ_15649번_N과M(1)_Backtracking.py
+++ jieun^,^/BOJ_15649번_N과M(1)_Backtracking.py
@@ -1,8 +1,5 @@
 N, M = map(int, input().split())    # 1 ~ N까지의 자연수 중 중복없이 M개를 고른 수열을 모두 구하여라
 
-visited = [False] * N   # 0 ~ N-1 방문여부
-listN = [i for i in range(N)]
-
 def Backtrack(num, ans, visited):
     
     # 해당 num 방문 처리 & 답에 추가
@@ -24,6 +21,9 @@ def Backtrack(num, ans, visited):
             visited[next_num]=False
             ans.remove(next_num+1)
 
+
+            
+visited = [False] * N   # 0 ~ N-1 방문여부
 
 for n in range(N):
     Backtrack(n, [], visited)
