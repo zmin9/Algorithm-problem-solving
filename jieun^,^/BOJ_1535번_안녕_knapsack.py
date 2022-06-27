2efdce68a5af241d153682606a8befc30a403585
Chae Jieun <86587287+cje1903@users.noreply.github.com>
2022-06-27 16:47:26 +0900

    06/27/월

--- jieun^,^/BOJ_1535번_안녕_knapsack.py
+++ jieun^,^/BOJ_1535번_안녕_knapsack.py
@@ -1,7 +1,7 @@
 # 체력과 기쁨 교환하는 배낭 문제
 # N-1번째 사람부터 시작해서 전 사람에게 인사하는 것과 인사하지 않는 경우의 이득을 재귀적으로 따져보기
 
-def knapsack(health, n, L, J):  # health: 남은 체력 / n: 사람 인덱스 / L: 잃는 체력 / J: 얻는 기쁨
+def knapsack(health, n, L, J):  # health: 남은 체력 / n: 사람 인덱스 / L: 잃는 체력 리스트 / J: 얻는 기쁨 리스트
     
     # 남은 체력이 0이거나
     if health==0 or n==0:
